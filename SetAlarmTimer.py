515991774cd1db4a5df1ad8d937a1756dba35a2d
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-13 14:19:23 +0900

    Create SetAlarmTimer.py

---
+++ SetAlarmTimer.py
@@ -0,0 +1,44 @@
+import datetime
+import time
+
+class SetAlarmTimer:
+    
+    def __init__(self, tm):
+
+        #Get Now
+        dt_now = datetime.datetime.now()
+        
+        if dt_now.hour > 0 and dt_now.hour <= tm[0]:
+            #Set Time
+            tmp = dt_now
+        else:
+            #Plus One day
+            tmp = dt_now + datetime.timedelta(days=1)
+            
+        self.AlarmTime = datetime.datetime(tmp.year,
+                                           tmp.month,
+                                           tmp.day,
+                                           tm[0],
+                                           tm[1],
+                                           0,0)
+        print(self.AlarmTime)
+
+    def RunAlarmTimer(self):
+        
+        while True:
+            if datetime.datetime.now() > self.AlarmTime:
+                print("Alem!!!")
+                break
+            else:
+                time.sleep(1)
+
+
+if __name__ == "__main__":        
+
+    #Set Hour and minites
+    tm = [7,30]
+
+    #Set Alarm
+    wt = SetAlarmTimer(tm)
+    #Run Alarm
+    wt.RunAlarmTimer()
