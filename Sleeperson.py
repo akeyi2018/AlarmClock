832d4ed20e7f476fdacf950c9e4eed62a8b94c3a
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-19 13:14:24 +0900

    Create Sleeperson.py

---
+++ Sleeperson.py
@@ -0,0 +1,11 @@
+class humanInfo:
+
+    def __init__(self):
+
+        self.age = [10,20,30,40,50,60,70,80]
+        self.gender = ["Male","Femail", "other"]
+        self.occupation = [""]        
+
+    def getSleepingrism(self):
+        pass
+        return True
