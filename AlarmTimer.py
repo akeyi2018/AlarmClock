4b72cb706e1941ffe0941d5fae3c78796b6f1601
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-19 16:36:10 +0900

    Update AlarmTimer.py

--- AlarmTimer.py
+++ AlarmTimer.py
@@ -1,12 +1,16 @@
 import datetime
 import time
+from SleepSchedule import schedule
 
 class AlarmTimer:
     
-    def __init__(self, tm):
-
+    def __init__(self, tm, flag):
+        
+        #Force flag
+        self.flag = flag
+        
         #Get Now
-        self.dt_now = datetime.datetime.now()
+        dt_now = datetime.datetime.now()
         
         if dt_now.hour > 0 and dt_now.hour <= tm[0]:
             #Set Time
@@ -14,13 +18,25 @@ class AlarmTimer:
         else:
             #Plus One day
             tmp = dt_now + datetime.timedelta(days=1)
+
+        #Get Holiday Info
+        if self.flag == 1:
+            self.isOffWork = False
+        else:
+            self.isOffWork = schedule(tmp.date()).IsOffWork()
             
         self.AlarmTime = datetime.datetime(tmp.year, tmp.month, tmp.day, tm[0], tm[1], 0, 0)
-        print(self.AlarmTime)
+    
 
     def RunAlarmTimer(self):
+
+        if self.isOffWork:
+            return 0
+
+        #print("Alarm Set:", self.AlarmTime)
         
         while True:
+            
             if datetime.datetime.now() > self.AlarmTime:
                 print("Alem!!!")
                 break
@@ -30,10 +46,9 @@ class AlarmTimer:
 
 if __name__ == "__main__":        
 
-    #Set Hour and minites
-    tm = [7,30]
-
     #Set Alarm
-    wt = AlarmTimer(tm)
+    wt = AlarmTimer([7,30],0)
+    
     #Run Alarm
     wt.RunAlarmTimer()
+
