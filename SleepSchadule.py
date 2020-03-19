c7714f945d11fcd01cf1939eddf5265fd4dc7c4a
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-19 13:13:01 +0900

    Create SleepSchadule.py

---
+++ SleepSchadule.py
@@ -0,0 +1,14 @@
+class scadule:
+
+    def __init__(self, requst):
+        
+        self.weekFlag = [1,1,1,1,1,0,0]
+        self.holiday = []
+        self.requst = requst
+        
+    def getScadule(self):
+
+        #compare weekflag
+        #compare holiday
+        pass
+        return True
