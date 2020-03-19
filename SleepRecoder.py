62c1c7a12c6270af701ee410a913595a625ada7a
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-19 13:11:49 +0900

    Create SleepRecoder.py

---
+++ SleepRecoder.py
@@ -0,0 +1,13 @@
+class sleepRecoder:
+
+    def __init__(self):
+        self.recordFile = recordFile
+
+    def recordSleepTime(self):
+        #record sleep time
+        pass
+
+    def recordWakeupTime(self):
+        #record wakeup time
+        pass
+        
