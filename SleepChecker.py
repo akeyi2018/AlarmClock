ff9634168a181de7fcdf7f25704696d35fcc3442
akeyi <34048522+akeyi2018@users.noreply.github.com>
2021-02-17 22:12:36 +0900

    Update SleepChecker.py

--- SleepChecker.py
+++ SleepChecker.py
@@ -1,16 +1,19 @@
-import RPi.GPIO as GPIO
+import pigpio
+from time import sleep
 
 class sleepChecker:
 
     def __init__(self, pin):
-
 	self.pin = pin
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM)
+	pi = pigpio.pi() 
+	pi.set_mode(self.pin, pigpio.OUTPUT) 
+	pi.write(self.pin, 1)
+        sleep(1.0)
+	pi.write(self.pin, 0)
 
-        GPIO.setup(self.pin, GPIO.IN, GPIO.PUD_UP)
-	GPIO.add_event_detect(pin, GPIO.RISING, bouncetime=1000)
-        GPIO.add_event_callback(pin, cb_setSleep)
+#         GPIO.setup(self.pin, GPIO.IN, GPIO.PUD_UP)
+# 	GPIO.add_event_detect(pin, GPIO.RISING, bouncetime=1000)
+#         GPIO.add_event_callback(pin, cb_setSleep)
 
     def cb_setSleep(self, channel):
 
