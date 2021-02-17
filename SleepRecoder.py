f8b24d1e3c7f5f371549804d00e67502a6c403fe
akeyi <34048522+akeyi2018@users.noreply.github.com>
2021-02-17 22:01:59 +0900

    Update SleepRecoder.py

--- SleepRecoder.py
+++ SleepRecoder.py
@@ -1,7 +1,10 @@
+import os
+
 class sleepRecoder:
 
-    def __init__(self):
-        self.recordFile = recordFile
+    def __init__(self, path):
+        self.path = path
+        self.config_json_file = os.path.join(self.path, 'config.json')
 
     def recordSleepTime(self):
         #record sleep time
